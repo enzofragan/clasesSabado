cefb73745a03aeef33d8f3f7cf795a6041026236
enzofragan <enzofragan@gmail.com>
2018-05-26 10:59:04 -0300

    archivos estructuras con lista y estados

--- clase-sabado-01/main.c
+++ clase-sabado-01/main.c
@@ -3,6 +3,17 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    FILE* f;
+    char texto[50]="los pelos verdes de agustin, ponte la gorra";
+
+    f=fopen("miHTML.html","w");
+
+    /**fprintf(f,"<html><body><h1>");
+    fprintf(f,"%s",texto);
+    fprintf(f,"</h1></body></html>");*/
+
+    fprintf(f,"<html><body><h1>%s</h1></body></html>",texto);
+
+    fclose(f);
     return 0;
 }
---
+++ clase-sabado-02/main.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct
+{
+    int id;
+    char nombre[50];
+    int estado;
+}ePersona;
+
+int main()
+{
+    ePersona listaPesonas[4] = {{1,"juan",1},{2,"maria",0},{3,"jose",1},{0,"",0}};
+    ePersona lectura[4];
+    int i;
+    FILE* f;
+
+    f=fopen("miArchivo.dat","wb");
+
+    for(i=0;i<4;i++)///para guardar los que tiene estado 1
+    {
+        if(listaPesonas[i].estado==1)///alta uno por uno
+        {
+            fwrite(&listaPesonas[i],sizeof(ePersona),1,f);///&cuando es un solo valor de estructura
+        }
+    }
+
+    fclose(f);
+
+
+    f=fopen("miArchivo.dat","rb");
+
+    fread(lectura,sizeof(ePersona),4,f);
+
+    fclose(f);
+
+    for(i=0;i<4;i++)
+    {
+        if(lectura[i].estado==1)///mostrar uno por uno con estado 1
+        {
+            printf("%d---%s\n",lectura[i].id,lectura[i].nombre);
+        }
+    }
+    return 0;
+}
